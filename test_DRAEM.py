2dee0948f84fbf7cac612d4aa96aac96b41ada33
ex3ploiter <52802071+ex3ploiter@users.noreply.github.com>
2023-02-15 13:14:54 +0330

    xa

--- test_DRAEM.py
+++ test_DRAEM.py
@@ -7,7 +7,7 @@ from sklearn.metrics import roc_auc_score, average_precision_score
 from model_unet import ReconstructiveSubNetwork, DiscriminativeSubNetwork
 import os
 
-def write_results_to_file(run_name, image_auc, pixel_auc, image_ap, pixel_ap):
+def write_results_to_file(run_name, image_auc, image_ap, pixel_ap):
     if not os.path.exists('./outputs/'):
         os.makedirs('./outputs/')
 
@@ -16,21 +16,6 @@ def write_results_to_file(run_name, image_auc, pixel_auc, image_ap, pixel_ap):
         fin_str += "," + str(np.round(i, 3))
     fin_str += ","+str(np.round(np.mean(image_auc), 3))
     fin_str += "\n"
-    fin_str += "pixel_auc,"+run_name
-    for i in pixel_auc:
-        fin_str += "," + str(np.round(i, 3))
-    fin_str += ","+str(np.round(np.mean(pixel_auc), 3))
-    fin_str += "\n"
-    fin_str += "img_ap,"+run_name
-    for i in image_ap:
-        fin_str += "," + str(np.round(i, 3))
-    fin_str += ","+str(np.round(np.mean(image_ap), 3))
-    fin_str += "\n"
-    fin_str += "pixel_ap,"+run_name
-    for i in pixel_ap:
-        fin_str += "," + str(np.round(i, 3))
-    fin_str += ","+str(np.round(np.mean(pixel_ap), 3))
-    fin_str += "\n"
     fin_str += "--------------------------\n"
 
     with open("./outputs/results.txt",'a+') as file:
@@ -38,9 +23,9 @@ def write_results_to_file(run_name, image_auc, pixel_auc, image_ap, pixel_ap):
 
 
 def test(obj_names, mvtec_path, checkpoint_path, base_model_name):
-    obj_ap_pixel_list = []
-    obj_auroc_pixel_list = []
-    obj_ap_image_list = []
+    
+    
+    
     obj_auroc_image_list = []
     for obj_name in obj_names:
         img_dim = 256
@@ -84,11 +69,50 @@ def test(obj_names, mvtec_path, checkpoint_path, base_model_name):
             true_mask = sample_batched["mask"]
             true_mask_cv = true_mask.detach().numpy()[0, :, :, :].transpose((1, 2, 0))
 
-            gray_rec = model(gray_batch)
-            joined_in = torch.cat((gray_rec.detach(), gray_batch), dim=1)
+            # gray_rec = model(gray_batch)
 
-            out_mask = model_seg(joined_in)
+            try:
+                # model.cuda()
+                if(cpuModelFlag==1):
+                    model.cuda()
+                    cpuModelFlag=0
+
+                gray_rec = model(gray_batch)
+                # model.cpu()
+            except:
+                model.cpu()
+                gray_rec = model(gray_batch.cpu())
+                cpuModelFlag=1
+
+            
+            # joined_in = torch.cat((gray_rec.detach(), gray_batch), dim=1)
+
+            try:
+                joined_in = torch.cat((gray_rec.detach(), gray_batch), dim=1)
+            except:
+                joined_in = torch.cat(
+                    (gray_rec.cpu().detach(), gray_batch.cpu()), dim=1)            
+
+
+            # out_mask = model_seg(joined_in)
+
+            try:
+                # model_seg.cuda()
+                if(cpuSegModelFlag==1):
+                    model.cuda()
+                    cpuSegModelFlag=0            
+
+                out_mask = model_seg(joined_in)
+                
+            except:
+                model_seg.cpu()
+                out_mask = model_seg(joined_in.cpu())
+                cpuSegModelFlag=1
+
+
             out_mask_sm = torch.softmax(out_mask, dim=1)
+
+            
 
 
             if i_batch in display_indices:
@@ -100,7 +124,7 @@ def test(obj_names, mvtec_path, checkpoint_path, base_model_name):
                 cnt_display += 1
 
 
-            out_mask_cv = out_mask_sm[0 ,1 ,: ,:].detach().cpu().numpy()
+            
 
             out_mask_averaged = torch.nn.functional.avg_pool2d(out_mask_sm[: ,1: ,: ,:], 21, stride=1,
                                                                padding=21 // 2).cpu().detach().numpy()
@@ -108,40 +132,24 @@ def test(obj_names, mvtec_path, checkpoint_path, base_model_name):
 
             anomaly_score_prediction.append(image_score)
 
-            flat_true_mask = true_mask_cv.flatten()
-            flat_out_mask = out_mask_cv.flatten()
-            total_pixel_scores[mask_cnt * img_dim * img_dim:(mask_cnt + 1) * img_dim * img_dim] = flat_out_mask
-            total_gt_pixel_scores[mask_cnt * img_dim * img_dim:(mask_cnt + 1) * img_dim * img_dim] = flat_true_mask
-            mask_cnt += 1
+
 
         anomaly_score_prediction = np.array(anomaly_score_prediction)
         anomaly_score_gt = np.array(anomaly_score_gt)
         auroc = roc_auc_score(anomaly_score_gt, anomaly_score_prediction)
-        ap = average_precision_score(anomaly_score_gt, anomaly_score_prediction)
-
-        total_gt_pixel_scores = total_gt_pixel_scores.astype(np.uint8)
-        total_gt_pixel_scores = total_gt_pixel_scores[:img_dim * img_dim * mask_cnt]
-        total_pixel_scores = total_pixel_scores[:img_dim * img_dim * mask_cnt]
-        auroc_pixel = roc_auc_score(total_gt_pixel_scores, total_pixel_scores)
-        ap_pixel = average_precision_score(total_gt_pixel_scores, total_pixel_scores)
-        obj_ap_pixel_list.append(ap_pixel)
-        obj_auroc_pixel_list.append(auroc_pixel)
+        
+        
         obj_auroc_image_list.append(auroc)
-        obj_ap_image_list.append(ap)
+        
         print(obj_name)
         print("AUC Image:  " +str(auroc))
-        print("AP Image:  " +str(ap))
-        print("AUC Pixel:  " +str(auroc_pixel))
-        print("AP Pixel:  " +str(ap_pixel))
         print("==============================")
 
     print(run_name)
     print("AUC Image mean:  " + str(np.mean(obj_auroc_image_list)))
-    print("AP Image mean:  " + str(np.mean(obj_ap_image_list)))
-    print("AUC Pixel mean:  " + str(np.mean(obj_auroc_pixel_list)))
-    print("AP Pixel mean:  " + str(np.mean(obj_ap_pixel_list)))
+    
 
-    write_results_to_file(run_name, obj_auroc_image_list, obj_auroc_pixel_list, obj_ap_image_list, obj_ap_pixel_list)
+    write_results_to_file(run_name, obj_auroc_image_list)
 
 if __name__=="__main__":
     import argparse
